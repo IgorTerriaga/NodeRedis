7eb89f61c6fa73704df6d7ab20cda83fd0bfe436
Igor Terriaga <igsantos1996@gmail.com>
2024-04-10 10:15:21 -0300

    Start APi Node with Redis. Usinf Elephant SQL and seed data with prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { fa, faker } from '@faker-js/faker';
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+async function main() {
+    const users = [];
+
+    for (let i = 0; i < 1000; i++) {
+        users.push({
+            name: faker.person.fullName(),
+            email: faker.internet.email()
+        })
+    }
+
+    // const userdata = users.map(user => ({
+    //     name: user.name,
+    //     email: user.email
+    // }));
+    await prisma.user.createMany({
+        data: users
+    });
+}
+
+main().catch(
+    (e) => {
+        console.error(e);
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
